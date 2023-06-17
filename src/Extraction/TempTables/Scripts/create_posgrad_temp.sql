431683967148b71cc4ac6896b98a9751ddd89a72
calimaco <viniciuschagas@protonmail.com>
2023-06-17 00:49:03 -0300

    fix: corrige nome area e nome programa de posgraduacoes

--- src/Extraction/TempTables/Scripts/create_posgrad_temp.sql
+++ src/Extraction/TempTables/Scripts/create_posgrad_temp.sql
@@ -10,31 +10,107 @@ SELECT
 INTO #filtered
 FROM AGPROGRAMA ap
 WHERE YEAR(ap.dtaselpgm) >= 2007
-	AND ap.codare BETWEEN 8000 AND 9000
+	AND ap.codare BETWEEN 8000 AND 8999
 	AND ap.vinalupgm = 'REGULAR';
 
 
--- Join NOMEAREA, AREA, NOMECURSO to get specifications
-SELECT
+-- Get transfers
+SELECT 
+	t.codpes AS 'numero_usp'
+	,t.codare AS 'codigo_area'
+	,t.numseqpgm AS 'seq_programa'
+	,h.dtaocopgm AS 'data_transferencia'
+INTO #transferencias
+FROM HISTPROGRAMA h
+	INNER JOIN TRANSFERAREA t
+		ON h.codpes = t.codpes
+			AND h.codare = t.codareori
+			AND h.numseqpgm = t.numseqpgmori
+			AND h.codaretrf = t.codare
+WHERE t.codare BETWEEN 8000 AND 8999;
+
+SELECT 
 	f.*
-    ,n.nomare AS 'nome_area'
-	,a.codcur AS 'codigo_programa'
-	,n2.nomcur AS 'nome_programa'
-INTO #specs
+	,CASE WHEN t.data_transferencia IS NOT NULL
+		THEN 'Transferência'
+		ELSE 'Selecionado'
+		END AS 'tipo_admissao'
+	,CASE WHEN t.data_transferencia IS NOT NULL
+		THEN t.data_transferencia
+		ELSE f.data_selecao
+		END AS 'data_admissao'
+INTO #admission
 FROM #filtered f
-    LEFT JOIN NOMEAREA n
-        ON f.codigo_area = n.codare
-            AND f.data_selecao >= n.dtainiare 
-            AND (n.dtafimare >= f.data_selecao OR n.dtafimare IS NULL)
-    LEFT JOIN AREA a
-        ON f.codigo_area = a.codare
-    LEFT JOIN NOMECURSO n2
-        ON a.codcur = n2.codcur
-            AND f.data_selecao >= n2.dtainicur
-            AND (n2.dtafimcur >= f.data_selecao OR n2.dtafimcur IS NULL);
+	LEFT JOIN #transferencias t
+		ON f.numero_usp = t.numero_usp
+			AND f.codigo_area = t.codigo_area
+			AND f.seq_programa = t.seq_programa;
 
 
--- Join HISTPROGRAMA to get first enrollment
+-- Get AREA names
+SELECT *
+INTO #areas
+FROM NOMEAREA na
+WHERE na.codare BETWEEN 8000 AND 8999;
+
+
+-- Get CURSO names
+SELECT *
+INTO #programas
+FROM NOMECURSO nc
+WHERE nc.codcur BETWEEN 8000 AND 8999;
+
+
+-- Alter AREA e CURSO names validity
+UPDATE #areas
+SET dtainiare = '1900-01-01'
+WHERE dtainiare = (
+    SELECT MIN(a2.dtainiare)
+    FROM #areas a2
+    WHERE a2.codare = #areas.codare
+);
+
+UPDATE #areas
+SET dtafimare = DATEADD(day, 1, GETDATE())
+WHERE dtainiare = (
+    SELECT MAX(a2.dtainiare)
+    FROM #areas a2
+    WHERE a2.codare = #areas.codare
+);
+
+UPDATE #programas
+SET dtainicur = '1900-01-01'
+WHERE dtainicur = (
+    SELECT MIN(p2.dtainicur)
+    FROM #programas p2
+    WHERE p2.codcur = #programas.codcur
+);
+
+UPDATE #programas
+SET dtafimcur = DATEADD(day, 1, GETDATE())
+WHERE dtainicur = (
+    SELECT MAX(p2.dtainicur)
+    FROM #programas p2
+    WHERE p2.codcur = #programas.codcur
+);
+
+-- Join specifications
+SELECT
+	adm.*
+    ,a.nomare AS 'nome_area'
+	,p.codcur AS 'codigo_programa'
+	,p.nomcur AS 'nome_programa'
+INTO #specs
+FROM #admission adm
+    LEFT JOIN #areas a
+        ON adm.codigo_area = a.codare
+            AND adm.data_admissao BETWEEN a.dtainiare AND a.dtafimare
+    LEFT JOIN #programas p
+        ON a.codcur = p.codcur
+            AND adm.data_admissao BETWEEN p.dtainicur AND p.dtafimcur;
+
+
+-- Join HISTPROGRAMA to get student's first enrollment
 SELECT
     s.*
     ,jn.dtaocopgm AS 'primeira_matricula'
@@ -70,7 +146,7 @@ SELECT
 		END AS 'data_ocorrencia'
 INTO #ocorrencias
 FROM HISTPROGRAMA h3
-WHERE h3.codare BETWEEN 8000 AND 9000;
+WHERE h3.codare BETWEEN 8000 AND 8999;
 
 
 -- Get last occurrence from occurrences table
@@ -121,6 +197,10 @@ FROM #primeira_matricula p
 
 -- Drop all unnecessary temp tables
 DROP TABLE #filtered;
+DROP TABLE #transferencias;
+DROP TABLE #admission;
+DROP TABLE #areas;
+DROP TABLE #programas;
 DROP TABLE #specs;
 DROP TABLE #primeira_matricula;
 DROP TABLE #ocorrencias;
